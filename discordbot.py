d66ffb92322511d0f9c6e6cd0cfc66d13031b806
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-07-20 23:14:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,8 +11,9 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 
 Channel_ID1 = 864848975139700736 #ログ
-Channel_ID2 = 864846769351294976 #注意ユーザーリスト
-Channel_ID3 = 865917109123809291 #サーバーステータス
+Channel_ID2 = 867042310180962315 #警戒ユーザーリスト
+Channel_ID3 = 864846769351294976 #注意ユーザーリスト
+Channel_ID4 = 865917109123809291 #サーバーステータス
 
 @bot.event
 async def on_ready():
@@ -21,7 +22,12 @@ async def on_ready():
     await channel.send('D.I.C.O.起動しました。')
     channel = bot.get_channel(Channel_ID2)
     await channel.purge()
-    embed = discord.Embed(title='注意ユーザーリスト',color=0xff0000)
+    embed = discord.Embed(title='⚠️警戒ユーザーリスト',color=ffff00)
+    embed.add_field(name='<@523369028920541194>',value='事前告知のないeveryoneメンションでマジックショーの予告をする。\n配慮に欠けた行為であり、6サーバーでの同様の行為が報告されているが、確認したサーバーではeveryoneメンションの禁止がルールに規定されていなかったことを考慮して、警戒Lv1に分類する。'
+    await channel.send(embed=embed)
+    channel = bot.get_channel(Channel_ID3)
+    await channel.purge()
+    embed = discord.Embed(title='⛔️注意ユーザーリスト',color=0xff0000)
     await channel.send(embed=embed)
 
 @bot.event
