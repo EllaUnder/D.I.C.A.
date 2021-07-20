618409df1ee1d3fba25e1d26ceb22cd8a7b39d7b
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-07-20 23:42:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,8 +11,8 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 
 Channel_ID1 = 864848975139700736 #ログ
-Channel_ID2 = 867042310180962315 #警戒ユーザーリスト
-Channel_ID3 = 864846769351294976 #注意ユーザーリスト
+Channel_ID2 = 867042310180962315 #注意ユーザーリスト
+Channel_ID3 = 864846769351294976 #警戒ユーザーリスト
 Channel_ID4 = 865917109123809291 #サーバーステータス
 
 @bot.event
@@ -23,13 +23,13 @@ async def on_ready():
     #警戒ユーザーリスト更新
     channel = bot.get_channel(Channel_ID2)
     await channel.purge()
-    embed = discord.Embed(title='⚠️警戒ユーザーリスト')
-    embed.add_field(name='<@523369028920541194>',value='事前告知のないeveryoneメンションでマジックショーの予告をする。\n配慮に欠けた行為であり、6サーバーでの同様の行為が報告されているが、確認したサーバーではeveryoneメンションの禁止がルールに規定されていなかったことを考慮して、警戒度Lv1に分類する。')
+    embed = discord.Embed(title='⚠️注意ユーザーリスト')
+    embed.add_field(name='<@523369028920541194>',value='事前告知のないeveryoneメンションでマジックショーの予告をする。\n配慮に欠けた行為であり、6サーバーでの同様の行為が報告されているが、確認したサーバーではeveryoneメンションの禁止がルールに規定されていなかったことを考慮して、注意Lv1に分類する。\nhttps://discord.com/channels/864768192399278110/864846073050431498/867049641232695336')
     await channel.send(embed=embed)
     #注意ユーザーリスト更新
     channel = bot.get_channel(Channel_ID3)
     await channel.purge()
-    embed = discord.Embed(title='⛔️注意ユーザーリスト',color=0xff0000)
+    embed = discord.Embed(title='⛔️警戒ユーザーリスト',color=0xff0000)
     await channel.send(embed=embed)
 
 @bot.event
