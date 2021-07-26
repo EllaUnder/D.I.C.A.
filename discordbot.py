a1131c9078aac23d48438c4a35177815aa483e3a
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-07-26 12:41:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -50,7 +50,7 @@ async def ping(ctx):
     
 @bot.command()
 async def give(ctx):
-    embed = discord.Embed(title=' ',description='[このチャンネルの上部へ](https://discord.com/channels/864768192399278110/864831620208656394/869026188805439488)')
+    embed = discord.Embed(description='[このチャンネルの上部へ](https://discord.com/channels/864768192399278110/864831620208656394/869026188805439488)')
     await ctx.send(embed=embed)
 
 
