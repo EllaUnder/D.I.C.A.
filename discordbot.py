ca0400ecd1e5b50ca8e245b7d5e46594e321024f
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-08-02 09:34:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -38,6 +38,7 @@ async def on_ready():
     embed.add_field(name='<@839856314414137354>',value='スパム行為による荒らし。\n警戒Lv1に分類する。')
     embed.add_field(name='<@716212058445709362>',value='Discordサーバー「荒らし連合」の招待リンクのスパム。\nしかし、荒らし連合との関連性は不明。\n警戒Lv1に分類。')
     embed.add_field(name='<@371687418346340352>',value='スパム行為による荒らし。\nセルフbotによる全チャンネルへ同じメッセージをスパム。\n警戒Lv2に分類する。')
+    embed.add_field(name='<@871053537193386064>',value='複数回にわたる個人メンションと人種差別発言。\n私怨か無差別か判断が付きにくいが、脅威度分類により警戒Lv2に分類する。\n[詳細](https://discord.com/channels/864768192399278110/864846073050431498/871549370816929843)')
     await channel.send(embed=embed)
 
 @bot.event
