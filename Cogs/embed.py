599f6c54e10a274b17092e9bc9fcb9893444a5fb
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-09-07 00:04:18 +0900

    Update embed.py

--- Cogs/embed.py
+++ Cogs/embed.py
@@ -1,1 +1,15 @@
+import discord
+from discord.ext import commands
 
+class embed(commands.Cog)
+    def __init__(self, bot):
+        self.bot = bot
+
+        @commands.command()
+        @commands.has_role(864846474399711253)
+        async def Elink(ctx,arg1,arg2):
+            embed = discord.Embed(description=f'[{arg1}]({arg2})')
+            await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(embed(bot)) 
