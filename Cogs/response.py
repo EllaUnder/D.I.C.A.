2a0cfd2d951c637f5d62a39dad23c1fa492f0040
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-09-07 00:11:08 +0900

    Update response.py

--- Cogs/response.py
+++ Cogs/response.py
@@ -1,1 +1,14 @@
+import discord
+from discord.ext import commands
+import random
 
+@bot.event
+async def on_message(message):
+    if message.content == 'おはよう' or message.content == 'オハヨウ' or 'おは' in message.content or 'オハヨー' in message.content:
+        luck = random.random()
+        if luck <= 0.3:
+            await message.channel.send('おはようございます。')
+        else:
+            pass
+
+    await bot.process_commands(message)
