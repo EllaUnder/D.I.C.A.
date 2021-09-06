ac29a690cc2582c2f9edc74d06c10d60b54ccc58
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-09-07 00:20:21 +0900

    Update response.py

--- Cogs/response.py
+++ Cogs/response.py
@@ -2,13 +2,20 @@ import discord
 from discord.ext import commands
 import random
 
-@bot.event
-async def on_message(message):
-    if message.content == 'おはよう' or message.content == 'オハヨウ' or 'おは' in message.content or 'オハヨー' in message.content:
-        luck = random.random()
-        if luck <= 0.3:
-            await message.channel.send('おはようございます。')
-        else:
-            pass
+class response(commands.Cog):
+    def __init__(self,bot):
+        self.bot =bot
 
-    await bot.process_commands(message)
+        @commands.Cog.listener()
+        async def on_message(self,message):
+            if message.content == 'おはよう' or message.content == 'オハヨウ' or 'おは' in message.content or 'オハヨー' in message.content:
+                luck = random.random()
+                if luck <= 0.3:
+                    await message.channel.send('おはようございます。')
+                else:
+                    pass
+
+        await bot.process_commands(message)
+
+def setup(bot):
+    bot.add_cog(response(bot))
