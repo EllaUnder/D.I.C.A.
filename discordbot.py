dd229704ca1467825b67ab62d323fcc39ff1fa84
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-09-07 18:13:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -94,6 +94,17 @@ async def Elink(ctx,arg1,arg2):
 
    
 #時報
+@bot.event
+async def on_message(message):
+    if message.content == 'おはよう' or message.content == 'オハヨウ' or 'おは' in message.content or 'オハヨー' in message.content:
+        luck = random.random()
+        if luck <= 0.3:
+            await message.channel.send('おはようございます。')
+        else:
+            pass
+
+await bot.process_commands(message)
+
 
 
 
