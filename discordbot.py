521770b4e69cf4d2f10d749bce6297f469ab1858
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-09-08 09:29:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,6 +24,7 @@ async def on_ready():
     print('起動しました')
     channel = bot.get_channel(Channel_ID1)
     await channel.send('D.I.C.O.起動しました。\nreginの実行を忘れないでください。')
+    timeloop.start()
 
 @bot.event
 async def on_command_error(ctx,error):
@@ -93,7 +94,7 @@ async def Elink(ctx,arg1,arg2):
     await ctx.send(embed=embed)
 
    
-#時報
+#レスポンス
 @bot.event
 async def on_message(message):
     if message.content == 'おはよう' or message.content == 'オハヨウ' or 'おは' in message.content or 'オハヨー' in message.content:
@@ -104,5 +105,16 @@ async def on_message(message):
             pass
 
     await bot.process_commands(message)
+
+#時報
+@tasks.loop(seconds=60)
+async def timeloop():
+    channel = bot.get_channel(881121615339986964) #ラウンジ
+    Greeting_List = ['今日も一日頑張りましょう。','オペレーターの皆さん、おはようございます。']
+    JST = timezone(timedelta(hours=+9),'JST')
+    now = datetime.datetime.now(JST).strftime('%H:%M')
+    if now == '09:00':
+        Today_Greeting = random.choice(Greeting_List)
+        await channel.send(f'D.I.C.O.が9時をお知らせします。\n{Today_Greeting}')
 
 bot.run(token)
