d66f3802e0617cdaad51e396b9f41a89db4f5628
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-09-14 09:43:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,6 +7,7 @@ import re
 import datetime
 from datetime import timedelta,timezone
 
+client = discord.Client()
 bot = commands.Bot(
     command_prefix='#d',
     strip_after_prefix = True,
@@ -102,6 +103,8 @@ async def Elink(ctx,arg1,arg2):
 #レスポンス
 @bot.event
 async def on_message(message):
+    if message.auther = client.user:
+        return
 
     if message.content == 'おはよう' or message.content == 'オハヨウ' or 'おは' in message.content or 'オハヨー' in message.content:
         luck = random.random()
