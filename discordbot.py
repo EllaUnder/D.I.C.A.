1b518634cd899cb5f17811097803d78050b66636
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-10-11 18:28:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -127,8 +127,11 @@ async def on_member_join(member):
     now = datetime.datetime.now()
     c_time = now - creared_at(member)
     u_name = name(member)
-    if c_time <= 2628002.88 and if 'ワッパステイ' in u_name or '共栄圏' in u_name:
-        await member.add_roles(884218829151043594) # user => member
+    if c_time <= 2628002.88:
+        if 'ワッパステイ' in u_name or if '共栄圏' in u_name:
+            await member.add_roles(884218829151043594) # user => member
+    else:
+        return
 
 #Embed
 @bot.command()
