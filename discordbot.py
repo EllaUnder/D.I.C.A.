a5636cbbef5ca955a314c4cab17a9276b4f1192e
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-10-15 23:09:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -148,7 +148,7 @@ async def Elink(ctx,arg1,arg2):
     #検索機能
 @bot.command()
 @commands.has_role(864846474399711253)
-async def search(ctx,arg1)
+async def search(ctx,arg1):
     if arg1 in list_txt:
         await ctx.send('検索ヒットしました。該当IDは報告リストに存在します。')
     else:
