be9999d8d45d3fd62ee8057001c8761ee77f797d
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-10-17 00:00:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -161,7 +161,7 @@ async def search(ctx,arg):
 @bot.event
 async def on_message(message):
     if message.content.startswith('Laplace') or message.content.startswith('ラプラス'):
-        await ctx.send('お呼びでしょうか？')
+        await message.channel.send('お呼びでしょうか？')
     
     await bot.process_commands(message)
 
