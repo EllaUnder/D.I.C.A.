af65aaad4875744449d3a541af40a3a7acaed230
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-10-16 23:31:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -161,6 +161,16 @@ async def search(ctx,arg):
 @bot.event
 
 #レスポンス
+@bot.event
+async def on_message(message):
+    if message.content == 'おはよう' or message.content == 'オハヨウ' or 'おは' in message.content or 'オハヨー' in message.content:
+        luck = random.random()
+        if luck <= 0.3:
+            await message.channel.send('おはようございます。')
+        else:
+            return
+
+    await bot.process_commands(message)
 
 
 #時報
