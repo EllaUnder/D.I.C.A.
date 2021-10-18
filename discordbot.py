6ef962028f2aa1c2d6342bb59d8adcfba9fe6a5c
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-10-18 14:04:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -198,9 +198,9 @@ async def on_message(message):
 @bot.command()
 @commands.has_any_role(865030088477900811,865029743173828608,864846474399711253)
 async def record(ctx,arg1,arg2):
-    r_operator = ctx.author
+    r_operator_mention = f"<@{ctx.author.id}>"
     embed = discord.Embed(title=f'{arg1}',description=f'{arg2}',color=0x00FF7F)
-    embed.set_author(name=f'{r_operator.mention}の活動報告')
+    embed.add_field(value=f'{r_operator_mention}の活動報告')
     channel = bot.get_channel(Channel_ID6)
     await channel.send(embed=embed)
     
