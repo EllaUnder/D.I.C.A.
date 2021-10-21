51701d466af3d6d6f16c2626728ab852e91b61b9
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-10-21 20:23:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -189,11 +189,11 @@ async def MsearchD(ctx,arg1,arg2):
 @bot.command()
 async def help(ctx):
     embed = discord.Embed(title='Laplaceのコマンド一覧',description='プレフィックスは`#d`です。\n引数`<>`は必須になります。引数`[]`はオプションです。',color=0x00ff8d)
-    embed.add_field(name='`search <ユーザーID>`',value='報告リスト、ブラックリストに指定したユーザーが存在するかどうか検索します。')
-    embed.add_field(name='`Elink <タイトル> <メッセージリンク>`',value='埋め込みメッセージを作成します。\n管理者専用です。')
-    embed.add_field(name='`MsearchD <検索範囲> <ユーザーID>`',value='検索範囲の中に指定したユーザーのメッセージがあった場合全て削除します。\n課長・副課長専用です。')
-    embed.add_field(name='`record <タイトル> <内容> [写真]`',value='荒らし対策の活動記録を残します。\nオフィサー専用です。')
-    embed.add_field(name='`help`',value='コマンド一覧を表示します。')
+    embed.add_field(name='__search <ユーザーID>__',value='報告リスト、ブラックリストに指定したユーザーが存在するかどうか検索します。')
+    embed.add_field(name='__Elink <タイトル> <メッセージリンク>__',value='埋め込みメッセージを作成します。\n管理者専用です。')
+    embed.add_field(name='__MsearchD <検索範囲> <ユーザーID>__',value='検索範囲の中に指定したユーザーのメッセージがあった場合全て削除します。\n課長・副課長専用です。')
+    embed.add_field(name='__record <タイトル> <内容> [写真]__',value='荒らし対策の活動記録を残します。\nオフィサー専用です。')
+    embed.add_field(name='__help__',value='コマンド一覧を表示します。')
     await ctx.send(embed=embed)
 
 #レスポンスコマンド
