dedd63738fca0a0f4562f4e756b57a4723a46e05
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-10-29 21:46:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -66,7 +66,6 @@ async def regin(ctx):
         channel = bot.get_channel(Channel_ID3)
         await channel.purge()
         embed = discord.Embed(title='⛔️警戒ユーザーリスト1',color=0xff0000)
-        embed.add_field(name='<@482484875794972692>',value='サーバー招待リンクのスパム。\n警戒Lv1に分類する。')
         embed.add_field(name='<@621546963963346956>',value='スパム行為による荒らし。\n警戒Lv1に分類する。')
         embed.add_field(name='<@839856314414137354>',value='スパム行為による荒らし。\n警戒Lv1に分類する。')
         embed.add_field(name='<@716212058445709362>',value='Discordサーバー「荒らし連合」の招待リンクのスパム。\nしかし、荒らし連合との関連性は不明。\n警戒Lv1に分類。')
