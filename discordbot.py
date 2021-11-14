4e5103d027a1d57ba7f66bca2471f98050d686f6
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-14 16:47:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -108,32 +108,34 @@ async def regin(ctx):
 #セキュリティシステム
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(864846240428457994) #ロビー
-    join_member_mention = f'<@{member.id}>'
-    await channel.send(f'ようこそ、{join_member_mention}様。貴方の入館を歓迎します。\nここはDiscord安全情報機関 D.I.C.A. ロビーです。\nまずは<#{864831620208656394}>と<#{864849667114926141}>をお読み下さい。')
+    g_id = member.guild.id
+    if g_id == 864768192399278110:
+        channel = bot.get_channel(864846240428457994) #ロビー
+        join_member_mention = f'<@{member.id}>'
+        await channel.send(f'ようこそ、{join_member_mention}様。貴方の入館を歓迎します。\nここはDiscord安全情報機関 D.I.C.A. ロビーです。\nまずは<#{864831620208656394}>と<#{864849667114926141}>をお読み下さい。')
     
-    user_id = str(member.id)
-    for join in r_json:
-        if user_id in join['id']:
-            await member.ban()
-            channel = bot.get_channel(Channel_ID5)
-            await channel.send('BANしました')
-            return
+        user_id = str(member.id)
+        for join in r_json:
+            if user_id in join['id']:
+                await member.ban()
+                channel = bot.get_channel(Channel_ID5)
+                await channel.send('BANしました')
+                return
 
-    #コンディションシステム
-    now = datetime.datetime.now()
-    c_time = now - member.created_at
-    u_name = member.name
-    if c_time.total_seconds() <= 2628002.88:
-        channel = bot.get_channel(Channel_ID1)
-        role = member.guild.get_role(884218829151043594)
-        await member.add_roles(role)
-        await channel.send('コンディション更新、カラーオレンジです。')
-    elif '共栄圏' in u_name or 'ワッパステイ' in u_name or '荒らし' in u_name or 'サウロン' in u_name:
-        await member.ban()
-        await channel.send('コンディション更新、カラーレッドです。')
-    else:
-        return
+        #コンディションシステム
+        now = datetime.datetime.now()
+        c_time = now - member.created_at
+        u_name = member.name
+        if c_time.total_seconds() <= 2628002.88:
+            channel = bot.get_channel(Channel_ID1)
+            role = member.guild.get_role(884218829151043594)
+            await member.add_roles(role)
+            await channel.send('コンディション更新、カラーオレンジです。')
+        elif '共栄圏' in u_name or 'ワッパステイ' in u_name or '荒らし' in u_name or 'サウロン' in u_name:
+            await member.ban()
+            await channel.send('コンディション更新、カラーレッドです。')
+        else:
+            return
 
 #コマンド
     #Embed
