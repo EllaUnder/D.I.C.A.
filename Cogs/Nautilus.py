b50c1b48b06a5d11f6a15a81f8341f963ccf2fc4
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-21 19:31:55 +0900

    Update Nautilus.py

--- Cogs/Nautilus.py
+++ Cogs/Nautilus.py
@@ -141,3 +141,6 @@ class Nautilus(commands.Cog):
         await ctx.send('全工程オールクリア。')
         time.sleep(random.uniform(0.5,1.0))
         await ctx.send('ギルド複製、完了を確認。\nお疲れ様でした。')
+
+def setup(bot)
+    bot.add_cog(Nautilus(bot))
