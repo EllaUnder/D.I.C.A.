fbd4124b0fae1c3f7c7dd96d5542886d8d28b7f5
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-21 22:14:44 +0900

    Update flame_event.py

--- Cogs/flame_event.py
+++ Cogs/flame_event.py
@@ -10,7 +10,7 @@ class Flame_Event(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener(name='on_ready')
+    @commands.Cog.listener()
     async def ready(self):
         print('起動しました')
         channel = self.bot.get_channel(Channel_ID1)
@@ -22,7 +22,7 @@ class Flame_Event(commands.Cog):
         time.sleep(random.uniform(0.5,1.5))
         await channel.send('安全保障機関 D.I.C.A.管制補佐システムLaplace、起動します。\nreginの実行を忘れないでください。')
 
-    @commands.Cog.listener(name='on_command_error')
+    @commands.Cog.listener()
     async def command_error(self,ctx,error):
         orig_error = getattr(error, "original", error)
         error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
