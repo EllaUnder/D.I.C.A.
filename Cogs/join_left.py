4a9da7cbf6c42b3d46dc0a6f7090f10ce96db991
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-21 18:18:22 +0900

    Update join_left.py

--- Cogs/join_left.py
+++ Cogs/join_left.py
@@ -2,11 +2,11 @@ class Join_Left(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
 
-    @commands.Cog.listener(name='on_message')
-    async def on_member_join(member):
+    @commands.Cog.listener(name='on_member_join')
+    async def on_join(self,member):
     g_id = member.guild.id
     if g_id == 864768192399278110:
-        channel = bot.get_channel(864846240428457994) #ロビー
+        channel = self.bot.get_channel(864846240428457994) #ロビー
         join_member_mention = f'<@{member.id}>'
         await channel.send(f'ようこそ、{join_member_mention}様。貴方の入館を歓迎します。\nここはDiscord安全情報機関 D.I.C.A. ロビーです。\nまずは<#{864831620208656394}>と<#{864849667114926141}>をお読み下さい。')
     
@@ -14,7 +14,7 @@ class Join_Left(commands.Cog):
         for join in r_json:
             if user_id in join['id']:
                 await member.ban()
-                channel = bot.get_channel(Channel_ID5)
+                channel = self.bot.get_channel(Channel_ID5)
                 await channel.send('BANしました')
                 return
 
@@ -22,8 +22,8 @@ class Join_Left(commands.Cog):
         now = datetime.datetime.now()
         c_time = now - member.created_at
         u_name = member.name
+        channel = self.bot.get_channel(Channel_ID1)
         if c_time.total_seconds() <= 2628002.88:
-            channel = bot.get_channel(Channel_ID1)
             role = member.guild.get_role(884218829151043594)
             await member.add_roles(role)
             await channel.send('コンディション更新、カラーオレンジです。')
