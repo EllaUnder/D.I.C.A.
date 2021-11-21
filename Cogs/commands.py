a029fe88162fb17ab78fbb522cd484d800923d2c
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-21 18:24:38 +0900

    Update commands.py

--- Cogs/commands.py
+++ Cogs/commands.py
@@ -3,3 +3,81 @@ bot.command()
 async def Elink(ctx,arg1,arg2):
     embed = discord.Embed(description=f'[{arg1}]({arg2})')
     await ctx.send(embed=embed)
+
+s_class = 'E','E-','E+','D','D-','D+','C','C-','C+','B','B-','B+','A','A-','A+','S','S-','S+'
+
+@bot.command()
+async def search(ctx,arg): 
+    if re.search('[a-zA-Z]',arg):
+        if arg not in s_class: # もし引数が予想以外なら警告で返す
+            await ctx.send(f"脅威クラス**{arg}**は存在しません。")
+            return
+
+        users_c = [] # クラスが一致した人の情報を入れておく
+
+        for info in r_json:  
+            if arg in info["class"]: # 正規表現じゃなくてinにすればEならEとE-,E+も入るし、E+ならE+だけが入る
+                users_c.append([info["id"], info["value"]]) # usersに情報を一旦保管
+
+        if users == []: # もし結果が空なら返す
+            await ctx.send("Search result: **None**")
+            await ctx.send('該当するユーザーは見つかりませんでした。')
+            return
+
+        embed = discord.Embed(title=f'脅威クラス**{arg}**の報告リスト',color=0xff0000) # 初期Embed
+        field_count = 0
+
+        for user in users_c:
+            if field_count >= 25: # いつもの
+                time.sleep(random.uniform(3.0,5.0))
+                await ctx.send(embed=embed)
+                embed = discord.Embed(title=f'驚異クラス"{arg}"の報告リスト',color=0xff0000)
+                field_count = 0
+                user_id = str(user[0])
+                user_content = str(user[1])
+                embed.add_field(name=f'▼__{user_id}__',value=user_content)
+
+            else:
+                user_id = str(user[0])
+                user_content = str(user[1])
+                field_count += 1
+                embed.add_field(name=f'▼__{user_id}__',value=user_content)
+
+        if field_count != 0:
+            await ctx.send(embed=embed)
+    
+    elif re.search('[0-9]',arg):
+        arg_digits = len(str(arg))
+        if 1 <= arg_digits <= 3:
+            users_d_value = []
+            
+    elif arg in r_list_txt:
+        await ctx.send('ちょっと待ってくださいね…')
+        time.sleep(random.uniform(0.5,1.5))
+        await ctx.send('該当IDは報告リストに存在します。')
+    
+    elif arg in b_list_txt:
+        await ctx.send('ちょっと待ってくださいね…')
+        time.sleep(random.uniform(0.5,1.5))
+        await ctx.send('該当IDはブラックリストに存在します。')
+
+    elif not arg in r_list_txt and not arg in b_list_txt:
+        await ctx.send('ちょっと待ってくださいね…')
+        time.sleep(random.uniform(0.5,1.5))
+        await ctx.send('該当IDは報告リスト・ブラックリストに存在しません。')
+
+    else:
+        return
+
+    #特定ユーザーのメッセージを削除
+@bot.command()
+@commands.has_any_role(864846474399711253,865029743173828608,899474219623129168)
+async def MsearchD(ctx,arg1,arg2):
+    channel = bot.get_channel(ctx.message.channel.id)
+    await ctx.send('サーチイテレータ、開始します…')
+    messages = await channel.history(limit=int(arg1)).flatten()
+    for message in messages:
+        if message.author.id == int(arg2):
+            await message.delete() # 検索対象のIDと一緒ならの処理
+    await ctx.send('悪いメッセージはドーン、ドン！💣💥')
+
