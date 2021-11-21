7b45ad93c08728f8e53ea9773b9ce076daa7677a
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-21 22:15:10 +0900

    Update commands.py

--- Cogs/commands.py
+++ Cogs/commands.py
@@ -5,6 +5,10 @@ import json
 import re
 import time
 
+with open("report.json",'r') as r:
+    r_json = json.load(r)
+    r_list_txt = r.read()
+
 with open("tarot.json",'r') as t:
     t_json = json.load(t)
     t_list = list(t_json.keys())
