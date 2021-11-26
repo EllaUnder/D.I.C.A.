5761f1e89b5502692906ba7418be54ac5bd0df04
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-26 17:51:27 +0900

    Update Server_security.py

--- Cogs/Server_security.py
+++ Cogs/Server_security.py
@@ -2,8 +2,8 @@ from discord.ext import commands
 import discord
 import re
 
-pattern1 = re.compile([\w-]{24}\.[\w-]{6}\.[\w-]{27})
-pattern2 = re.compile(mfa\.[\w-]{84})
+pattern1 = r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}"
+pattern2 = r"mfa\.[\w-]{84}"
 
 class SSecurity(commands.Cog):
     def __init__(self,bot):
@@ -12,7 +12,7 @@ class SSecurity(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self,message):
         m_ = message.content
-        if pattern1.search(pattern1,m_) or pattern2.search(pattern2,m_):
+        if re.search(pattern1,m_) or re.search(pattern2,m_):
             await message.delete()
             print ('fire')
 
