bed9ac4f216dd59f34c429164e92d1ffc1de8ef1
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-27 08:29:10 +0900

    Update commands.py

--- Cogs/commands.py
+++ Cogs/commands.py
@@ -20,22 +20,32 @@ def searcharg(args):
         if re.search('[a-zA-Z]',arg):
             if arg not in s_class:
                 return "process1" #脅威クラスは存在しません
-            elif re.search('[0-9]',arg): #評価値アバウト
+            elif arg in s_class:
                 return "process2"
-            elif len(arg) == 18: #ID報告書あり
+        elif re.search('[0-9]',arg): #評価値アバウト
+            digits = len(arg)
+            if not 1<=digits<=3:
+                return "process5"
+            elif 1<=digits<=3:
                 return "process3"
+        elif len(arg) == 18: #ID報告書あり
+            return "process4"
+        else:
+            return "process5"
 
     if factor_len == 2:
         arg1 = ','.join(args)[0]
         arg2 = ','.join(args)[1]
         if arg1 != ["just","j","existence","e"] or arg2 != ["just","j","existence","e"]: 
-            return "process4" #想定されていない引数
+            return "process5" #想定されていない引数
         elif re.search('[0-9]',arg1) or re.search('[0-9]',arg2): 
             if arg1 = ["just","j"] or arg2 = ["just","j"]: #評価値ピッタリ
-                return "process5"
+                return "process6"
         elif len(arg1) == 18 or len(arg2) == 18:
             if arg1 = ["existence","e"] or arg2 = ["existence","e"]: #ID存在のみ
-                return "process6"
+                return "process7"
+        else:
+            return "process5"
 
 class Commands(commands.Cog):
     def __init__(self,bot):
@@ -50,30 +60,21 @@ class Commands(commands.Cog):
 
     #検索
     @commands.command()
-    async def search(self,ctx,*arg):
-        tag = searcharg()
+    async def search(self,ctx,*args):
+        tag = searcharg(args)
         if tag == "process1":
             await ctx.send(f'脅威クラス{arg}は存在しません。')
-        elif tag == "process2"
-    @commands.command()
-    async def search(self,ctx,arg):
-        if re.search('[a-zA-Z]',arg):
-            if arg not in s_class: # もし引数が予想以外なら警告で返す
-                await ctx.send(f"脅威クラス**{arg}**は存在しません。")
-                return
 
+        elif tag == "process2":
             for info in r_json:  
                 if arg in info["class"]: # 正規表現じゃなくてinにすればEならEとE-,E+も入るし、E+ならE+だけが入る
                     users_c.append([info["id"], info["value"]]) # usersに情報を一旦保管
-
             if users_c == []: # もし結果が空なら返す
                 await ctx.send("Search result: **None**")
                 await ctx.send('該当するユーザーは見つかりませんでした。')
                 return
-
             embed = discord.Embed(title=f'脅威クラス**{arg}**の報告リスト',color=0xff0000) # 初期Embed
             field_count = 0
-
             for user in users_c:
                 if field_count >= 25: # いつもの
                     time.sleep(random.uniform(3.0,5.0))
@@ -83,15 +84,27 @@ class Commands(commands.Cog):
                     user_id = str(user[0])
                     user_content = str(user[1])
                     embed.add_field(name=f'▼__{user_id}__',value=user_content)
-
                 else:
                     user_id = str(user[0])
                     user_content = str(user[1])
                     field_count += 1
                     embed.add_field(name=f'▼__{user_id}__',value=user_content)
-
             if field_count != 0:
                 await ctx.send(embed=embed)
+
+        elif tag == "process3":
+            
+
+
+
+    @commands.command()
+    async def search(self,ctx,arg):
+        if re.search('[a-zA-Z]',arg):
+            if arg not in s_class: # もし引数が予想以外なら警告で返す
+                await ctx.send(f"脅威クラス**{arg}**は存在しません。")
+                return
+
+           
     
         elif re.search('[0-9]',arg):
             arg_digits = len(str(arg))
