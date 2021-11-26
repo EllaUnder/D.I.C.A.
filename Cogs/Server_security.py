6f55acfae885bb3642637ba53fdcd319a2e3d06c
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-26 17:31:56 +0900

    Update Server_security.py

--- Cogs/Server_security.py
+++ Cogs/Server_security.py
@@ -1,7 +1,17 @@
 from discord.ext import commands
 import discord
+import re
+
+pattern1 = "[\w-]{24}\.[\w-]{6}\.[\w-]{27}"
+pattern2 = "mfa\.[\w-]{84}"
 
 class SSecurity(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self,message):
+        m_ = message.content
+        if re.search(pattern1,m_) or re.search(pattern2,m_):
+            await message.delete()
     
