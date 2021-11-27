54a2c32125cd74eab000e3187057dd30906de616
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-27 20:47:12 +0900

    Update commands.py

--- Cogs/commands.py
+++ Cogs/commands.py
@@ -60,7 +60,7 @@ class Commands(commands.Cog):
 
     #検索
     @commands.command()
-    async def search(self,ctx,*args):
+    async def Dsearch(self,ctx,*args):
         tag = searcharg(args)
         if tag == "process1":
             await ctx.send(f'脅威クラス{arg}は存在しません。')
