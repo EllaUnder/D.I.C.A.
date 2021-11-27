8c431bc165ac152a81d92e95d7aacc04e7cec4c6
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-28 00:20:14 +0900

    Update commands.py

--- Cogs/commands.py
+++ Cogs/commands.py
@@ -16,7 +16,7 @@ def countarg(args):
     factor_len = len(args)
 
     if factor_len == 1:
-        arg = ','.join(args)[0]
+        global arg = ','.join(args)[0]
         if re.search('[a-zA-Z]',arg):
             if arg not in s_class:
                 return "process1" #脅威クラスは存在しません
@@ -34,8 +34,8 @@ def countarg(args):
             return "process5"
 
     if factor_len == 2:
-        arg1 = ','.join(args)[0]
-        arg2 = ','.join(args)[1]
+        global arg1 = ','.join(args)[0]
+        global arg2 = ','.join(args)[1]
         if arg1 != ["just","j","existence","e"] or arg2 != ["just","j","existence","e"]: 
             return "process5" #想定されていない引数
         elif re.search('[0-9]',arg1) or re.search('[0-9]',arg2): 
@@ -66,7 +66,6 @@ class Commands(commands.Cog):
             await ctx.send(f'脅威クラス{arg}は存在しません。')
 
         elif tag == "process2":
-            arg = ','.join(args)[0]
             for info in r_json:  
                 if arg in info["class"]: # 正規表現じゃなくてinにすればEならEとE-,E+も入るし、E+ならE+だけが入る
                     users_c.append([info["id"], info["value"]]) # usersに情報を一旦保管
