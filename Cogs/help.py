162224e8c37b55cdfba1a6ba74b4912d57f6dd3c
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-29 20:17:39 +0900

    Update help.py

--- Cogs/help.py
+++ Cogs/help.py
@@ -7,7 +7,7 @@ class Help(commands.Cog):
 
     @commands.command()
     async def help(self,ctx):
-        embed = discord.Embed(title='Laplaceのコマンド一覧',description='プレフィックスは`#d`です。\n引数`<>`は必須になります。引数`[]`はオプションです。',color=0x00ff8d)
+        embed = discord.Embed(title='Laplaceのコマンド一覧',description='プレフィックスは`#l`です。\n引数`<>`は必須になります。引数`[]`はオプションです。',color=0x00ff8d)
         embed.add_field(name='__search <ユーザーID>["existence"or"e"]__',value='D.I.C.A.データベースを検索し、指定したユーザーの報告書を返します。\nオプション引数がある場合、「存在するかどうか」のみを返します。')
         embed.add_field(name='__search <脅威クラス>__',value='指定した脅威クラスの前後+-の報告書を返します。\n+か-まで指定されている場合、その脅威クラスのみを返します。\n("D"と検索すると"D-","D","D+"を返し、"D+"と検索すると"D+"のみ)')
         embed.add_field(name='__search <評価値>["just"or"j"]__',value='指定した評価値の+-5の範囲に認定された報告書を返します。\nオプション引数がある場合、「検索された評価値ピッタリ」の報告書を返します。')
