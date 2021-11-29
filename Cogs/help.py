119440de412dc93bfbd2ceec4e7184895d2759cd
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-29 17:03:41 +0900

    Update help.py

--- Cogs/help.py
+++ Cogs/help.py
@@ -13,7 +13,6 @@ class Help(commands.Cog):
         embed.add_field(name='__search <評価値>["just"or"j"]__',value='指定した評価値の+-5の範囲に認定された報告書を返します。\nオプション引数がある場合、「検索された評価値ピッタリ」の報告書を返します。')
         embed.add_field(name='__Elink <タイトル> <メッセージリンク>__',value='埋め込みメッセージを作成します。\n管理者専用です。')
         embed.add_field(name='__MsearchD <検索範囲> <ユーザーID>__',value='検索範囲の中に指定したユーザーのメッセージがあった場合全て削除します。\n課長・副課長専用です。')
-        embed.add_field(name='__record <タイトル> <内容> [写真]__',value='荒らし対策の活動記録を残します。\nオフィサー専用です。')
         embed.add_field(name='__help__',value='コマンド一覧を表示します。')
         await ctx.send(embed=embed)
 
