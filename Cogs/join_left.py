64c1be3c4f179c563dfc985d098118dee1545b93
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-29 20:08:33 +0900

    Update join_left.py

--- Cogs/join_left.py
+++ Cogs/join_left.py
@@ -20,29 +20,6 @@ class Join_Left(commands.Cog):
             channel = self.bot.get_channel(864846240428457994) #ロビー
             join_member_mention = f'<@{member.id}>'
             await channel.send(f'ようこそ、{join_member_mention}様。貴方の入館を歓迎します。\nここはDiscord安全情報機関 D.I.C.A. ロビーです。\nまずは<#{864831620208656394}>と<#{864849667114926141}>をお読み下さい。')
-    
-            user_id = str(member.id)
-            for join in r_json:
-                if user_id in join['id']:
-                    await member.ban()
-                    channel = self.bot.get_channel(Channel_ID5)
-                    await channel.send('BANしました')
-                    return
-
-            #コンディションシステム
-            now = datetime.datetime.now()
-            c_time = now - member.created_at
-            u_name = member.name
-            channel = self.bot.get_channel(Channel_ID1)
-            if c_time.total_seconds() <= 2628002.88:
-                role = member.guild.get_role(884218829151043594)
-                await member.add_roles(role)
-                await channel.send('コンディション更新、カラーオレンジです。')
-            elif '共栄圏' in u_name or 'ワッパステイ' in u_name or '荒らし' in u_name or 'サウロン' in u_name:
-                await member.ban()
-                await channel.send('コンディション更新、カラーレッドです。')
-            else:
-                return
 
     @commands.Cog.listener()
     async def on_member_remove(self,member):
