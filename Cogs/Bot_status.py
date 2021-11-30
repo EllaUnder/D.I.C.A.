25a6114f8b0fc79a9043b3e7479a107189bf4d7d
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-30 11:31:17 +0900

    Update Bot_status.py

--- Cogs/Bot_status.py
+++ Cogs/Bot_status.py
@@ -10,7 +10,7 @@ class Status(commands.Cog):
         if ctx.author.id == 854331482444267550:
             g_list = self.bot.guilds
             for info in g_list:
-                await ctx.send(info)
+                await ctx.send(f'{info}')
 
 def setup(bot):
     return bot.add_cog(Status(bot))
