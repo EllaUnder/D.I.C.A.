ae1a9b8b2ac1653a308a5d5b94d8492e550766c9
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-11-30 15:13:04 +0900

    Update Bot_status.py

--- Cogs/Bot_status.py
+++ Cogs/Bot_status.py
@@ -12,13 +12,14 @@ class Status(commands.Cog):
             for info in g_list:
                 Gid = info.id
                 Gname = info.name
-                await ctx.send(f'**{Gname}**(id:**{Gid}**)')
+                await ctx.send(f'{Gname}(id:**{Gid}**)')
 
     @commands.command()
     async def leave(self,ctx,arg):
         if ctx.author.id == 854331482444267550:
             guild = self.bot.get_guild(int(arg))
             await guild.leave()
+            await ctx.send(f'{guild.name}(id:**{guild.id}**)を去りました。')
             
 def setup(bot):
     return bot.add_cog(Status(bot))
