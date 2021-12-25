df1df45e1aba1aef1026800650d77fb15b856f2a
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2021-12-25 16:33:21 +0900

    Update Bot_status.py

--- Cogs/Bot_status.py
+++ Cogs/Bot_status.py
@@ -15,15 +15,15 @@ class Status(commands.Cog):
         Gid = guild.id
         Gicon_url = guild.icon_url
 
-        if str(Gid) in ibs_txt:
-            await guild.leave()
-            await channel.send(f'禁止指定サーバーに招待されたため自動退去しました。\n【D座標】\nサーバー名:{Gname}\nID:{Gid}')
-        
         embed = discord.Embed(title='Botが以下のサーバーに招待されました。',color=0x00ff8d)
         embed.add_field(name=f'サーバー名:{Gname}',value=f'サーバーID:**{Gid}**')
         embed.set_image(url=Gicon_url)
         await channel.send('<@854331482444267550>')
         await channel.send(embed=embed)
+
+        if str(Gid) in ibs_txt:
+            await guild.leave()
+            await channel.send(f'禁止指定サーバーに招待されたため自動退去しました。\n【D座標】\nサーバー名:{Gname}\nID:{Gid}')
 
     @commands.command()
     async def Gcheck(self,ctx):
