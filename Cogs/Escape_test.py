e0511113028d463ddea3476738e5c895385a5d3a
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2022-01-11 21:56:43 +0900

    Update Escape_test.py

--- Cogs/Escape_test.py
+++ Cogs/Escape_test.py
@@ -10,12 +10,14 @@ class Escape(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self,message):
-        m_s = messages.append(message.content)
+        if message.guild.id == 864768192399278110:
+            m_s = messages.append(message.content)
 
-        if len(m_s) = 2:
-            m_s1 = m_s[0]
-            m_s2 = m_s[1]
-            pass
+            if len(m_s) = 2:
+                m_s1 = m_s[0]
+                m_s2 = m_s[1]
+                res = difflib.ndiff(str_1.split(), str_2.split())
+                print('\n'.join(res))
 
 def setup(bot):
     return bot.add_cog(Escape(bot))
