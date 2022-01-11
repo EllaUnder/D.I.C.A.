a2e96630c3c899f560a52d286c5715e6005e1f1d
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2022-01-11 21:53:41 +0900

    Update Escape_test.py

--- Cogs/Escape_test.py
+++ Cogs/Escape_test.py
@@ -2,13 +2,20 @@ from discord.ext import commands
 import discord
 import difflib
 
+messages = {}
+
 class Escape(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self,message):
-        pass
+        m_s = messages.append(message.content)
+
+        if len(m_s) = 2:
+            m_s1 = m_s[0]
+            m_s2 = m_s[1]
+            pass
 
 def setup(bot):
     return bot.add_cog(Escape(bot))
