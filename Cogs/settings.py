b4b7aecc6832eb73066a4ccb1c75ff64baf8c873
EllaUnder <87417488+EllaUnder@users.noreply.github.com>
2022-04-17 18:13:45 +0900

    Update settings.py

--- Cogs/settings.py
+++ Cogs/settings.py
@@ -1,2 +1,9 @@
 from discord.ext import commands
 import discord
+
+class Settings(commands.Cog):
+  def __init__(self,bot):
+    self.bot = bot
+    
+def setup(bot):
+   return bot.add_cog(Settings(bot))
